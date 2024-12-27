f7d48908cb67ef23dffa00f08ddae4a0c680493a
tng00 <80770913+tng00@users.noreply.github.com>
2024-12-27 16:17:29 +0300

    cache recommendations

--- app/routers/search.py
+++ app/routers/search.py
@@ -9,6 +9,11 @@ from app.backend.db_depends import get_db
 from app.routers.auth import get_current_user
 from app.routers.category import handle_db_error
 from mai_recsys.main import mlic
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import Redis
+from fastapi_cache.decorator import cache
+
 router = APIRouter(prefix="/search", tags=["search"])
 templates = Jinja2Templates(directory="app/templates")
 
@@ -106,6 +111,12 @@ def build_filter_conditions(q, category_id, min_rating_value):
     return " AND ".join(filters), params
 
 
+redis = Redis(host='localhost', port=6379)
+
+FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+@app.get("/recommendations/{user_id}")
+@cache(expire=86400)
 @router.get("/", response_class=HTMLResponse)
 async def home(
     request: Request,
